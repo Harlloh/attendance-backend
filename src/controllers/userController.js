7f8787f28d23a904cd7732c61dc9f9cec9dffd0d
Harlloh <alloolorunfemi@gmail.com>
2026-04-30 15:10:42 +0100

    fixed the endpoint for generatin queue numbers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,104 @@
 import { prisma } from "../config/db.js";
+import { handleNumberAssignment } from "./adminController.js";
 
 export const getNumber = async (req, res) => {
-    const { stateCode, name } = req.body
+    try {
+        const { stateCode, name, browserId, checkInSlug } = req.body;
+        if (!stateCode || !name) {
+            return res.status(401).json({ success: false, message: 'Name and statecode are required.' })
+        }
+
+        if (!browserId) {
+            return res.status(401).json({ success: false, message: 'Please refresh your browser, unique id could not be generated for you.' })
+        }
+
+        const lga = await prisma.lGA.findUnique({
+            where: { checkInSlug },
+            include: {
+                sessions: { where: { isOpen: true }, take: 1 }
+            }
+        });
+        console.log('88', lga, 'this is the lga that gives the session id');
+        if (!lga || lga.sessions.length === 0) {
+            return res.status(400).json({ success: false, message: "No active session", status: 'no_session' });
+        }
+
+        const sessionId = lga.sessions[0].id
+
+        console.log(sessionId, 'Session Id');
+        // Check fingerprint first
+        const existingByDevice = await prisma.attendanceRecord.findUnique({
+            where: {
+                sessionId_deviceFingerprint: { sessionId, deviceFingerprint: browserId }
+            }
+        });
+        // console.log(existingByDevice, 'Existing device value');
+        if (existingByDevice) {
+            return res.status(200).json({
+                success: true,
+                alreadyCheckedIn: true,
+                queueNumber: existingByDevice.queueNumber,
+                checkedInAt: existingByDevice.timestamp,
+                name: existingByDevice.name,
+                status: 'already_in'
+
+            });
+        }
+
+        // Check state code
+        const existingByStateCode = await prisma.attendanceRecord.findUnique({
+            where: {
+                sessionId_stateCode: { sessionId, stateCode }
+            }
+        });
+        // console.log(existingByStateCode, 'existing by statecode');
+        if (existingByStateCode) {
+            return res.status(200).json({
+                success: true,
+                alreadyCheckedIn: true,
+                queueNumber: existingByStateCode.queueNumber,
+                checkedInAt: existingByStateCode.timestamp,
+                name: existingByStateCode.name,
+                status: 'already_in'
+
+            });
+        }
+        console.log(sessionId, stateCode, name, browserId);
+        const record = await handleNumberAssignment(false, sessionId, stateCode, name, browserId);
+        return res.status(201).json({
+            success: true,
+            alreadyCheckedIn: false,
+            queueNumber: record.queueNumber,
+            checkedInAt: record.timestamp,
+            name: record.name,
+            stateCode: record.stateCode,
+            status: 'success'
+        });
+
+    } catch (error) {
+        console.error('Error getting number:', error.message)
+        return res.status(500).json({ success: false, message: 'Internal server error', status: 'error' })
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export const validateSession = async (req, res) => {
     const { checkInSlug } = req.query;
